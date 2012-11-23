f06df2ced941ffd70420bc094a659abb3f784796
Jim Hayes <jhayes@sdsc.edu>
2012-11-23 18:55:44 +0000

    Clean up leftover rjags stuff.

--- src/r-modules/install_packages.sh
+++ src/r-modules/install_packages.sh
@@ -6,9 +6,6 @@ CRANURL=http://cran.stat.ucla.edu
 yum -y install curl-devel
 # R_LIBS might be needed for package dependencies
 export R_LIBS=${PKGROOT}/local/lib
-export LD_LIBRARY_PATH=/opt/lapack/gnu/lib:/opt/jags/lib:$LD_LIBRARY_PATH
-export JAGS_INCLUDE=/opt/jags/include/JAGS
-export JAGS_LIB=/opt/jags/lib
 mkdir -p ${R_LIBS}
 
 ${PKGROOT}/bin/R --vanilla << END
