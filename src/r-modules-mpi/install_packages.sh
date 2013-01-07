2b386e4551e736769f590f393434226d7925bc9a
Jim Hayes <jhayes@sdsc.edu>
2013-01-07 18:39:11 +0000

    Back out buggy changes.

--- src/r-modules-mpi/install_packages.sh
+++ src/r-modules-mpi/install_packages.sh
@@ -5,7 +5,7 @@ PKGROOT=${1:-/opt/R}
 MPIROOT=${2}
 ROLLMPI=${3}
 ROLLNETWORK=${4}
-OPENMPIROOT=${5}
+CRANURL=http://cran.stat.ucla.edu
 yum -y install curl-devel
 # R_LIBS might be needed for package dependencies
 export R_LIBS=${PKGROOT}/local/lib/mpi/${ROLLMPI}/${ROLLNETWORK}
@@ -13,7 +13,6 @@ mkdir -p ${R_LIBS}
 
 ${PKGROOT}/bin/R --vanilla << END
 # Specify where to pull package source from
-CRANURL=http://cran.stat.ucla.edu
 repos <- getOption("repos")
 repos["CRAN"] = "${CRANURL}"
 options(repos = repos)
@@ -22,16 +21,11 @@ mpiConfig <- c(
   "--with-mpi=${MPIROOT}"
 )
 localPackages <- c(
+  'Rmpi',
   'rlecuyer',
   'ncdf4'
 )
 for (package in localPackages) {
   install.packages(package, configure.args=mpiConfig, lib="${R_LIBS}", INSTALL_opts="--no-clean-on-error")
 }
-export R_LIBS=${PKGROOT}/local/lib/mpi/openmpi/${ROLLNETWORK}
-mpiConfig <- c(
-  "--with-mpi=${OPENMPIROOT}",
-  "--with-Rmpi-type=OPENMPI"
-)
-install.packages('Rmpi', configure.args=mpiConfig, lib="${R_LIBS}",INSTALL_opts="--no-clean-on-error")
 END
