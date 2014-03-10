fbc8a82056e9322490bef0afa1d4040435f5f7f9
Jim Hayes <jhayes@sdsc.edu>
2014-03-10 11:46:03 -0700

    Specify autoreq:no to rpm.

--- src/r-modules-mpi/version.mk
+++ src/r-modules-mpi/version.mk
@@ -1,2 +1,3 @@
 NAME = r-modules-mpi_$(ROLLMPI)_$(ROLLNETWORK)
 RELEASE = 0
+RPM.EXTRAS         = AutoReq:No
--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,3 +1,4 @@
 NAME	= r-modules-roll-test
 VERSION	= 1
 RELEASE	= 0
+RPM.EXTRAS         = AutoReq:No
