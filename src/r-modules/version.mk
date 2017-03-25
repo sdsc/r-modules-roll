6ccc5489a866baaf768c544c6869d4177dc2a94d
Jim Hayes <jhayes@sdsc.edu>
2017-03-25 12:33:32 -0700

    Restore compilation of fts, MCMCpack, and sem packages.
    Resolves APS-432.

--- src/r-modules/version.mk
+++ src/r-modules/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME       = sdsc-R-module-collection
 VERSION    = 2
-RELEASE    = 7
+RELEASE    = 8
 PKGROOT    = /opt/R/local/lib
 
 RPM.EXTRAS = AutoReq:No
